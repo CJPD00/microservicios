d0882bce8c46bdc76de4115636ff184cc61a4f07
CJPD00 <carlosjavierpacheco60@gmail.com>
2025-07-16 11:32:06 -0400

    u28

--- payments-ms/src/payments/payments.service.ts
+++ payments-ms/src/payments/payments.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { envs } from 'config/envs';
 import { PaymentSessionDto } from 'dto/payment-session.dto';
+import { Request, Response } from 'express';
 import Stripe from 'stripe';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
-  createPaymentSession(data: PaymentSessionDto) {
+  async createPaymentSession(data: PaymentSessionDto) {
     const { currency, items } = data;
     const lineItems = items.map((item) => ({
       price_data: {
@@ -20,7 +21,7 @@ export class PaymentsService {
       quantity: item.quantity,
     }));
 
-    const session = this.stripe.checkout.sessions.create({
+    const session = await this.stripe.checkout.sessions.create({
       payment_intent_data: {
         metadata: {},
       },
@@ -31,5 +32,14 @@ export class PaymentsService {
     });
 
     return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+    } catch (error) {}
   }
 }
